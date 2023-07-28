376e5ac818bb580f7e430c886bbbb7f62fc868fd
보니 <73548875+Guboneui@users.noreply.github.com>
2023-07-29 03:01:44 +0900

    [EDIT] case는 소문자로 시작하기

--- Sources/Toast/ToastDirection.swift
+++ Sources/Toast/ToastDirection.swift
@@ -1,13 +1,8 @@
-//
 //  ToastDirection.swift
-//  
-//
-//  Created by 구본의 on 2023/06/23.
-//
 
 import Foundation
 
 public enum ToastDirection {
-  case BottomToTop
-  case TopToBottom
+  case bottomToTop
+  case topToBottom
 }
